cfba52a8e938f68bdb01a4656e018972788c36ef
Ubuntu <ubuntu@ip-172-31-18-114.eu-central-1.compute.internal>
2017-01-04 07:46:59 +0000

    add hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xxiir(69$qu+gdvoo2(v(f_628s12so=@c*4o#qb&^o5^=7ww$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nikosvlagoidis.com','35.156.248.216']
 
 
 # Application definition
